9222d4f94ecef104cf3380ff6360479d1d532cb3
Saurabh <sumsau2022@gmail.com>
2025-05-03 01:12:41 +0530

    done registration view

--- api/views.py
+++ api/views.py
@@ -1,9 +1,19 @@
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
+from rest_framework import exceptions as rest
+
+from .serializers import *
 
 
 @api_view(['POST'])
 @permission_classes([AllowAny])
-def registerUser(request):
+def auth(request, action):
+    if action == 'register':
+        serializer = ManagementStaffSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=201)
+        
+
     
